d2167f4088f3747a610ecdcfc2f6cfaed69772a2
Burhan SAVCI <burhansavci@gmail.com>
2020-03-22 18:47:55 +0300

    Add EfCoreRepositoryBase
    Implement basic methods for Repository pattern

---
+++ aspnet-core/src/MyBlog.Core/DataAccess/EntityFrameworkCore/EfCoreRepositoryBase.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MyBlog.Core.DataAccess.EntityFrameworkCore
+{
+    public class EfCoreRepositoryBase<TEntity, TDbContext> : IRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TDbContext : DbContext, new()
+    {
+        private readonly DbContext _dbContext;
+        public virtual DbSet<TEntity> Table => _dbContext.Set<TEntity>();
+        public EfCoreRepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public TEntity Add(TEntity entity)
+        {
+            _dbContext.Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbContext.Set<TEntity>().FirstOrDefault(predicate);
+        }
+        public IQueryable<TEntity> GetAll()
+        {
+            return GetAllIncluding();
+        }
+        public IQueryable<TEntity> GetAllIncluding(params Expression<Func<TEntity, object>>[] propertySelectors)
+        {
+            var query = Table.AsQueryable();
+            if (propertySelectors.Length != 0)
+            {
+                foreach (var propertySelector in propertySelectors)
+                {
+                    query = query.Include(propertySelector);
+                }
+            }
+            return query;
+        }
+        public List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Where(predicate).ToList();
+        }
+        public List<TEntity> GetAllList()
+        {
+            return GetAll().ToList();
+        }
+        public TEntity Update(TEntity entity)
+        {
+            _dbContext.Update(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+    }
+}
