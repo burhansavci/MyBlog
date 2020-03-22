4668a0d2cd167d886a643408b990943fff78d122
Burhan SAVCI <burhansavci@gmail.com>
2020-03-22 18:46:49 +0300

    Add IRepository

---
+++ aspnet-core/src/MyBlog.Core/DataAccess/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MyBlog.Core.DataAccess
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> GetAllIncluding(params Expression<Func<TEntity, object>>[] propertySelectors);
+        List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate);
+        List<TEntity> GetAllList();
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        TEntity Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
