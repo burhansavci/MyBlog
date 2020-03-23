6c82727fbc3ba0b862a7651e4977eb7df07037c1
Burhan SAVCI <burhansavci@gmail.com>
2020-03-23 21:50:10 +0300

    Add CreatedDate to Category

--- aspnet-core/src/MyBlog.Entities/Concrete/Category.cs
+++ aspnet-core/src/MyBlog.Entities/Concrete/Category.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace MyBlog.Entities.Concrete
 {
     public class Category : Entity
     {
+        public DateTime CreatedDate { get; set; }
         public virtual ICollection<Article> Articles { get; set; }
         public virtual ICollection<CategoryTranslation> CategoryTranslations { get; set; }
     }
