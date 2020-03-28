844e4d522d025d11093b7d1589f6c1e77b02e1ec
Burhan SAVCI <burhansavci@gmail.com>
2020-03-28 21:49:31 +0300

    Add result models

---
+++ aspnet-core/src/MyBlog.Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,13 @@
+﻿namespace MyBlog.Core.Utilities.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data) : base(false, data)
+        {
+        }
+
+        public ErrorDataResult(string message, T data) : base(false, message, data)
+        {
+        }
+    }
+}
---
+++ aspnet-core/src/MyBlog.Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,13 @@
+﻿namespace MyBlog.Core.Utilities.Results
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        public SuccessDataResult(T data) : base(true, data)
+        {
+        }
+
+        public SuccessDataResult(string message, T data) : base(true, message, data)
+        {
+        }
+    }
+}
