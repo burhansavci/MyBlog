621ad01b9a99cba63bdce6ff92cefd6546f31184
Burhan SAVCI <burhansavci@gmail.com>
2020-03-29 20:33:04 +0300

    Add UserManager to MyBlog.Business

---
+++ aspnet-core/src/MyBlog.Business/Abstract/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using MyBlog.Core.Utilities.Results;
+using MyBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<User> GetByEmail(string email);
+        IDataResult<User> InsertUser(User user);
+    }
+}
---
+++ aspnet-core/src/MyBlog.Business/Concrete/UserManager.cs
@@ -0,0 +1,27 @@
+﻿using MyBlog.Business.Abstract;
+using MyBlog.Core.Utilities.Results;
+using MyBlog.DataAccess.Abstract;
+using MyBlog.Entities.Concrete;
+
+namespace MyBlog.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserRepository _userRepository;
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IDataResult<User> InsertUser(User user)
+        {
+           _userRepository.Insert(user);
+            return new SuccessDataResult<User>(user);
+        }
+
+        public IDataResult<User> GetByEmail(string email)
+        {
+            return new SuccessDataResult<User>(_userRepository.Get(x => x.Email == email));
+        }
+    }
+}
