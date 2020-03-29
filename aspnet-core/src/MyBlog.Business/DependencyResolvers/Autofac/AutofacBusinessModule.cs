cfade8a22c8c23778cb8772684e9dabd8e952a5d
Burhan SAVCI <burhansavci@gmail.com>
2020-03-29 20:32:28 +0300

    Add AutofacBusinessModule

---
+++ aspnet-core/src/MyBlog.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using MyBlog.DataAccess.Concrete.EntityFrameworkCore;
+using System.Linq;
+
+namespace MyBlog.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterType<MyBlogDbContext>().AsSelf().InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.Load(nameof(DataAccess)))
+                   .Where(t => t.Namespace.Contains("Repositories"))
+                   .As(t => t.GetInterfaces()[0]);
+
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.Load(nameof(Business)))
+                   .Where(t => t.Name.EndsWith("Manager"))
+                   .AsImplementedInterfaces();
+
+        }
+    }
+}
