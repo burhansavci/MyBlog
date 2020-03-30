b0e03f90f899f210f93075b9c412a12597b01fd9
Burhan SAVCI <burhansavci@gmail.com>
2020-03-30 21:22:47 +0300

    Add UserLoginDto and UserRegisterDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using MyBlog.Core.Entities;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class UserRegisterDto : IDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
