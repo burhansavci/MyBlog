7f6bd4e1479abd6ba492e8f4a98064bd1f918b1f
Burhan SAVCI <burhansavci@gmail.com>
2020-04-04 15:26:48 +0300

    Add helpers for security infrastructure

---
+++ aspnet-core/src/MyBlog.Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MyBlog.Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper
+    {
+        public static SecurityKey CreateSecurityKey(string securtiyKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securtiyKey));
+        }
+    }
+}
---
+++ aspnet-core/src/MyBlog.Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyBlog.Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                if (computedHash.Length != passwordHash.Length)
+                    return false;
+
+                for (int i = 0; i < computedHash.Length; i++)
+                    if (passwordHash[i] != computedHash[i])
+                        return false;
+            }
+            return true;
+        }
+    }
+}
