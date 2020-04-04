5d35d4638c89dca44a8b5880f542136f76a2def8
Burhan SAVCI <burhansavci@gmail.com>
2020-04-04 15:32:04 +0300

    Add Messages class to avoid magic strings

---
+++ aspnet-core/src/MyBlog.Business/Constants/Messages.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBlog.Business.Constants
+{
+    public static class Messages
+    {
+        public const string UserNotFound = "User was not found.";
+        public const string UserAlreadyExists = "User is already exists.";
+        public const string PasswordError = "Password is not correct.";
+        public const string SuccessfulLogin = "Login to the system is successful.";
+        public const string UserRegistered = "User successfully registered.";
+        public const string AccessTokenCreated = "AccessToken sucessfully created.";
+    }
+}
