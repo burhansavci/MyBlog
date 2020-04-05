10acbfe9516e1919c94012a43057721180aa823b
Burhan SAVCI <burhansavci@gmail.com>
2020-04-05 22:00:36 +0300

    Add AuthController

---
+++ aspnet-core/src/MyBlog.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Business.Abstract;
+using MyBlog.Entities.Dtos;
+
+namespace MyBlog.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("login")]
+        public IActionResult Login(UserLoginDto userLoginDto)
+        {
+            var userToLogin = _authService.Login(userLoginDto);
+            if (!userToLogin.Success) 
+                return BadRequest(userToLogin.Message);
+
+            var result = _authService.CreateAccessToken(userToLogin.Data);
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserRegisterDto userRegisterDto)
+        {
+            var userToRegister = _authService.Register(userRegisterDto);
+            if (!userToRegister.Success)
+                return BadRequest(userToRegister.Message);
+
+            var result = _authService.CreateAccessToken(userToRegister.Data);
+            if (!result.Success) 
+                return BadRequest(result.Message);
+
+            return Ok(result.Data);
+        }
+
+    }
+}
