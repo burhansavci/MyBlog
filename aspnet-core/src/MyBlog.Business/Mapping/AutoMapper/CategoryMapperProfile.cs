342a803b11a14cb1ad153d6fbac190e9f4299c2f
Burhan SAVCI <burhansavci@gmail.com>
2020-04-07 22:49:25 +0300

    Add AutoMapper

---
+++ aspnet-core/src/MyBlog.Business/Mapping/AutoMapper/CategoryMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MyBlog.Entities.Concrete;
+using MyBlog.Entities.Dtos;
+
+namespace MyBlog.Business.Mapping.AutoMapper
+{
+    public class CategoryMapperProfile : Profile
+    {
+        public CategoryMapperProfile()
+        {
+            CreateMap<CategoryTranslation, CategoryDto>().IncludeMembers(x => x.Language, x => x.Category);
+
+            CreateMap<Language, CategoryDto>(MemberList.None);
+            CreateMap<Category, CategoryDto>(MemberList.None);
+        }
+    }
+}
