9c97d97603685213c6cbb63ffe9585b6231fdb4d
Burhan SAVCI <burhansavci@gmail.com>
2020-04-09 21:55:42 +0300

    Add ILanguageService to MyBlog.Business

---
+++ aspnet-core/src/MyBlog.Business/Abstract/ILanguageService.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Core.Utilities.Results;
+using MyBlog.Entities.Dtos;
+using System.Collections.Generic;
+
+namespace MyBlog.Business.Abstract
+{
+    public interface ILanguageService
+    {
+        IDataResult<LanguageDto> GetLanguageByCode(string languageCode);
+        IDataResult<List<LanguageDto>> GetLanguages();
+        IResult InsertLanguage(LanguageDto languageDto);
+        IResult UpdateLanguage(LanguageDto languageDto);
+        IResult DeleteLanguage(LanguageDto languageDto);
+    }
+}
