8a1eb1bcb6a2b4d19ea1aa0c5e8b60c68dd3e0e6
Burhan SAVCI <burhansavci@gmail.com>
2020-04-09 21:53:24 +0300

    Add CategoriesController to MyBlog.WebAPI

---
+++ aspnet-core/src/MyBlog.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Business.Abstract;
+using MyBlog.Entities.Dtos;
+
+namespace MyBlog.WebAPI.Controllers
+{
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [Route("api/{languageCode}/[controller]/")]
+        [HttpGet]
+        public IActionResult GetCategories(string languageCode)
+        {
+            return Ok(_categoryService.GetCategories(languageCode));
+        }
+
+        [Route("api/{languageCode}/categories/{id}")]
+        [HttpGet]
+        public IActionResult GetCategoryById(int id, string languageCode)
+        {
+            return Ok(_categoryService.GetCategoryById(id, languageCode));
+        }
+
+        [Route("api/[controller]")]
+        [HttpPost]
+        public IActionResult AddCategory(CategoryDto categoryDto)
+        {
+            return Ok(_categoryService.InsertCategory(categoryDto));
+        }
+
+        [Route("api/[controller]")]
+        [HttpPut]
+        public IActionResult UpdateCategory(CategoryDto categoryDto)
+        {
+            return Ok(_categoryService.UpdateCategory(categoryDto));
+        }
+
+        [Route("api/[controller]")]
+        [HttpDelete]
+        public IActionResult DeleteCategory(CategoryDto categoryDto)
+        {
+            return Ok(_categoryService.DeleteCategory(categoryDto));
+        }
+    }
+}
