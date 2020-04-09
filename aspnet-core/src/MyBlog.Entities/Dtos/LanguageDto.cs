0527ddf7878dfdf82e128f16a4e1e5e372300bd6
Burhan SAVCI <burhansavci@gmail.com>
2020-04-09 21:54:41 +0300

    Add LanguageDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/LanguageDto.cs
@@ -0,0 +1,11 @@
+﻿using MyBlog.Core.Entities;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class LanguageDto : IDto
+    {
+        public string LanguageCode { get; set; }
+        public string Name { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
