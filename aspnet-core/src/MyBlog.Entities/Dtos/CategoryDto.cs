7b11845edf8f828c6d1f25fa94c56130f1b95f88
Burhan SAVCI <burhansavci@gmail.com>
2020-04-12 20:24:20 +0300

    Add Id field to CategoryDto and update CategoryId to nullable

--- aspnet-core/src/MyBlog.Entities/Dtos/CategoryDto.cs
+++ aspnet-core/src/MyBlog.Entities/Dtos/CategoryDto.cs
@@ -8,7 +8,8 @@ namespace MyBlog.Entities.Dtos
         public string LanguageCode { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int CategoryId { get; set; }
+        public int Id { get; set; }
+        public int? CategoryId { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
