0b773650d8ed19493dfff0a82b5b6d0c2e585517
Burhan SAVCI <burhansavci@gmail.com>
2020-04-12 20:30:19 +0300

    Update CategoryDto to CategoryTranslation and  CategoryDto to Category

--- aspnet-core/src/MyBlog.Business/Mapping/AutoMapper/CategoryMapperProfile.cs
+++ aspnet-core/src/MyBlog.Business/Mapping/AutoMapper/CategoryMapperProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using MyBlog.Entities.Concrete;
 using MyBlog.Entities.Dtos;
-using System;
 
 namespace MyBlog.Business.Mapping.AutoMapper
 {
@@ -9,14 +8,23 @@ namespace MyBlog.Business.Mapping.AutoMapper
     {
         public CategoryMapperProfile()
         {
-            CreateMap<CategoryTranslation, CategoryDto>().IncludeMembers(x => x.Category);
+            CreateMap<CategoryTranslation, CategoryDto>()
+                .IncludeMembers(x => x.Category);
+
             CreateMap<Category, CategoryDto>(MemberList.None);
 
 
-            CreateMap<CategoryTranslation, CategoryDto>()
-                .IncludeMembers( x => x.Category)
-                .ReverseMap();
-            CreateMap<CategoryDto, Category>(MemberList.None);
+            CreateMap<CategoryDto, Category>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.CategoryId));
+
+            CreateMap<CategoryDto, CategoryTranslation>()
+                .ForPath(d => d.Category.CreatedDate, opt => opt.MapFrom(s => s.CreatedDate))
+                .ForPath(d => d.Category.Id, opt => opt.MapFrom(s => s.CategoryId))
+                .AfterMap((s, d) =>
+                {
+                    if (s.CategoryId != null)
+                        d.Category = null;
+                });
         }
     }
 }
