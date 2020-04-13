f692db8166877f62b63039a33e94b482f52c23f4
Burhan SAVCI <burhansavci@gmail.com>
2020-04-13 20:51:07 +0300

    Add IArticleSerivce

---
+++ aspnet-core/src/MyBlog.Business/Abstract/IArticleService.cs
@@ -0,0 +1,16 @@
+﻿using MyBlog.Core.Utilities.Results;
+using MyBlog.Entities.Dtos;
+using System.Collections.Generic;
+
+namespace MyBlog.Business.Abstract
+{
+    public interface IArticleService
+    {
+        IDataResult<ArticleDto> GetArticleById(int id, string languageCode);
+        IDataResult<ArticleDto> GetArticlesByCategoryId(int categoryId, string languageCode);
+        IDataResult<List<ArticleDto>> GetArticles(string languageCode);
+        IResult InsertArticle(ArticleDto articleDto);
+        IResult UpdateArticle(ArticleDto articleDto);
+        IResult DeleteArticle(ArticleDto articleDto);
+    }
+}
