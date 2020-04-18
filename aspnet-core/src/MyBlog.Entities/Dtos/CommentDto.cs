a1d2413de1de9e14851819743764631b60403ec2
Burhan SAVCI <burhansavci@gmail.com>
2020-04-18 18:22:35 +0300

    Add CommentDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Core.Entities;
+using System;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class CommentDto : IDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContentMain { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int ArticleId { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
