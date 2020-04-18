954a59fc7ab172ecd9687d78d2cfee6e9d12f864
Burhan SAVCI <burhansavci@gmail.com>
2020-04-18 18:25:47 +0300

    Add ICommentService

---
+++ aspnet-core/src/MyBlog.Business/Abstract/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Core.Utilities.Results;
+using MyBlog.Entities.Dtos;
+using System.Collections.Generic;
+
+namespace MyBlog.Business.Abstract
+{
+    public interface ICommentService
+    {
+        IDataResult<List<CommentDto>> GetCommentsByArticleId(int articleId);
+        IDataResult<List<CommentDto>> GetComments();
+        IResult InsertComment(CommentDto commentDto);
+        IResult UpdateComment(CommentDto commentDto);
+        IResult DeleteComment(CommentDto commentDto);
+    }
+}
