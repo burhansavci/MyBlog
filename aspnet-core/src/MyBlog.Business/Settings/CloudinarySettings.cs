bb14f9791159659a51e8be1770ab4fbaacff8623
Burhan SAVCI <burhansavci@gmail.com>
2020-04-19 18:53:21 +0300

    Add CloudinarySettings

---
+++ aspnet-core/src/MyBlog.Business/Settings/CloudinarySettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Business.Settings
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string APIKey { get; set; }
+        public string APISecret { get; set; }
+    }
+}
