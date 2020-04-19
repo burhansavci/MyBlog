7377bc6f4752352b8d36ca9b5a34ccd47ba42487
Burhan SAVCI <burhansavci@gmail.com>
2020-04-19 18:52:52 +0300

    Add AppSettingsSection for appsettings.json file

---
+++ aspnet-core/src/MyBlog.Business/Constants/AppSettingsSection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Business.Constants
+{
+    public static class AppSettingsSection
+    {
+        public const string Cloudinary = "CloudinarySettings";
+    }
+}
