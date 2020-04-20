2bda5c06f1f4b7a73378b2a22e5944e70dc71a0c
Burhan SAVCI <burhansavci@gmail.com>
2020-04-20 21:45:35 +0300

    Add IPictureService

---
+++ aspnet-core/src/MyBlog.Business/Abstract/IPictureService.cs
@@ -0,0 +1,14 @@
+﻿using MyBlog.Core.Utilities.Results;
+using MyBlog.Entities.Dtos;
+using System.Collections.Generic;
+
+namespace MyBlog.Business.Abstract
+{
+    public interface IPictureService
+    {
+        IDataResult<PictureForReturnDto> GetPictureById(int id);
+        IDataResult<List<PictureForReturnDto>> GetPicturesByArticleId(int articleId);
+        IResult InsertPictureForArticle(PictureForCreationDto pictureDto);
+        IResult DeletePicture(PictureForDeleteDto pictureForDeleteDto);
+    }
+}
