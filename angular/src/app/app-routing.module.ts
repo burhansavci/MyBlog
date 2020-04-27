951f247aef241f6d39a10de2cd9c84f04b3957cd
Burhan SAVCI <burhansavci@gmail.com>
2020-04-27 23:27:57 +0300

    Update app-routing for new components

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,9 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: MainLayoutComponent, children: [] },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
