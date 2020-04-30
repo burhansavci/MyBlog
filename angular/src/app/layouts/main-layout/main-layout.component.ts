4709b70e200462fc672347a2e9319af4d0f536a1
Burhan SAVCI <burhansavci@gmail.com>
2020-04-30 22:32:26 +0300

    Update main-layout component

--- angular/src/app/layouts/main-layout/main-layout.component.ts
+++ angular/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.css']
+  styleUrls: ['./main-layout.component.css'],
 })
 export class MainLayoutComponent implements OnInit {
-
-  constructor() { }
+  isAsideActive: boolean = true;
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      if (
+        this.router.url.includes('about') ||
+        this.router.url.includes('contact')
+      ) {
+        this.isAsideActive = false;
+      } else {
+        this.isAsideActive = true;
+      }
+    });
   }
-
 }
