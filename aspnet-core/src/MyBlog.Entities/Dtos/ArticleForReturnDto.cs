a705680cb71b4dc071a0f1e439e720e6d6d84681
Burhan SAVCI <burhansavci@gmail.com>
2020-05-01 22:44:17 +0300

    Add new return Dtos for Article and Category

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/ArticleForReturnDto.cs
@@ -0,0 +1,19 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class ArticleForReturnDto : IDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ContentSummary { get; set; }
+        public string ContentMain { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int ViewCount { get; set; }
+        public string LanguageCode { get; set; }
+        public ICollection<CategoryForReturnDto> Categories { get; set; }
+        public ICollection<PictureForReturnDto> Pictures { get; set; }
+    }
+}
---
+++ aspnet-core/src/MyBlog.Entities/Dtos/CategoryForReturnDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class CategoryForReturnDto
+    {
+        public int Id { get; set; }
+        public string LanguageCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
