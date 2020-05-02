20d3e65907f1b8cd920f3612a7b8926e534ea22b
Burhan SAVCI <burhansavci@gmail.com>
2020-05-02 22:52:26 +0300

    Switched to using of ngx-bootstrap collapse

--- angular/src/app/nav/main-nav/main-nav.component.ts
+++ angular/src/app/nav/main-nav/main-nav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent implements OnInit {
-
+  isCollapsed = true;
   constructor() { }
 
   ngOnInit(): void {
