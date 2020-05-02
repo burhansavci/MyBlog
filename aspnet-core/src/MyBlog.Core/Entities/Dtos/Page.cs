2f6b602bdfe8fcf44d3c118746c2ab531ca7f763
Burhan SAVCI <burhansavci@gmail.com>
2020-05-02 22:24:34 +0300

    Switched using of Page Dto instead of PagedList

---
+++ aspnet-core/src/MyBlog.Core/Entities/Dtos/Page.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Core.Entities.Dtos
+{
+    public class Page<T> : IDto
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public List<T> Items { get; set; }
+
+        public Page(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            Items = items;
+        }
+
+        public static Page<T> CreatePaginatedResult(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var count = query.Count();
+            var items = query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new Page<T>(items, count, pageNumber, pageSize);
+        }
+
+        public static async Task<Page<T>> CreatePaginatedResultAsync(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var count = await query.CountAsync();
+            var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new Page<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
