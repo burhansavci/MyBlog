da1f340fa9eabd80d49f8fdce63a3689ca34d521
Burhan SAVCI <burhansavci@gmail.com>
2020-05-02 23:09:01 +0300

    Add article resolver

---
+++ angular/src/app/resolvers/article.resolver.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Article } from '../models/article';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { ArticleService } from '../services/article.service';
+import { AlertifyService } from '../services/alertify.service';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleResolver implements Resolve<Article[]> {
+  constructor(
+    private articleService: ArticleService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
+  pageNumber = 1;
+  pageSize = 5;
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Article[]> {
+
+    return this.articleService.getArticles(this.pageNumber, this.pageSize).pipe(
+      catchError(error => {
+        this.alertify.error('Problem retrieving articles data');
+        this.router.navigate(['/']);
+        return of(null);
+    })
+  );
+  }
+}
