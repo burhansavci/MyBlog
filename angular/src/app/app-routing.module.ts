058c31b3803e14f7b13e4a8a628eb5acffb2b104
Burhan SAVCI <burhansavci@gmail.com>
2020-05-03 18:40:22 +0300

    Added new paths and resolve for routings

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ArticleResolver } from './resolvers/article.resolver';
+import { ArticleComponent } from './pages/article/article.component';
 
 const routes: Routes = [
   {
@@ -13,6 +15,16 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent,
+        resolve: { dataResult: ArticleResolver },
+      },
+      {
+        path: 'page/:page',
+        component: HomeComponent,
+        resolve: { dataResult: ArticleResolver },
+      },
+      {
+        path: 'article/:title/:id',
+        component: ArticleComponent,
       },
       {
         path: 'about',
@@ -24,6 +36,7 @@ const routes: Routes = [
       },
     ],
   },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
