19cd3a51baad192e1a9d053fdea4e60ed13acab8
Burhan SAVCI <burhansavci@gmail.com>
2020-05-03 18:39:27 +0300

    Update app.module with new components and modules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +13,10 @@ import { FooterNavComponent } from './nav/footer-nav/footer-nav.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ArticleService } from './services/article.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ArticleComponent } from './pages/article/article.component';
+import { UrlformatPipe } from './pipes/urlformat.pipe';
 
 @NgModule({
   declarations: [
@@ -19,9 +27,21 @@ import { HomeComponent } from './pages/home/home.component';
     AboutComponent,
     ContactComponent,
     HomeComponent,
+    ArticleComponent,
+    UrlformatPipe,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PaginationModule.forRoot(),
+    CollapseModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [
+    ArticleService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
