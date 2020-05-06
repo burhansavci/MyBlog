b79dc937c239416280c4965ef8356fb35e211ca7
Burhan SAVCI <burhansavci@gmail.com>
2020-05-06 23:26:33 +0300

    Updated scrollPosition for components

--- angular/src/app/pages/home/home.component.ts
+++ angular/src/app/pages/home/home.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PaginatedResult } from 'src/app/models/paginated-result';
 import { Article } from 'src/app/models/article';
-import { AlertifyService } from 'src/app/services/alertify.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ArticleService } from 'src/app/services/article.service';
 import { DataResult } from 'src/app/models/data-result';
 
 @Component({
@@ -13,12 +11,7 @@ import { DataResult } from 'src/app/models/data-result';
 })
 export class HomeComponent implements OnInit {
   dataResult: DataResult<PaginatedResult<Article[]>>;
-  constructor(
-    private articleService: ArticleService,
-    private alertify: AlertifyService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.route.data.subscribe((data) => {
@@ -28,6 +21,5 @@ export class HomeComponent implements OnInit {
 
   pageChanged(event: any): void {
     this.router.navigate([`/page/${event.page}`]);
-    window.scrollTo(0, 0);
   }
 }
