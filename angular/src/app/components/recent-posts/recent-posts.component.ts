9605507e8822b34cca3bcdcdc08aba934274dde7
Burhan SAVCI <burhansavci@gmail.com>
2020-05-06 23:25:11 +0300

    Added recent-posts component

---
+++ angular/src/app/components/recent-posts/recent-posts.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { DataResult } from 'src/app/models/data-result';
+import { PaginatedResult } from 'src/app/models/paginated-result';
+import { Article } from 'src/app/models/article';
+import { ArticleService } from 'src/app/services/article.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
+
+@Component({
+  selector: 'app-recent-posts',
+  templateUrl: './recent-posts.component.html',
+  styleUrls: ['./recent-posts.component.css'],
+})
+export class RecentPostsComponent implements OnInit {
+  dataResult: DataResult<PaginatedResult<Article[]>>;
+  constructor(
+    private articleService: ArticleService,
+    private alertify: AlertifyService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadRecentPosts();
+  }
+  loadRecentPosts() {
+    this.articleService.getArticles(1, 5).subscribe(
+      (dataResult: DataResult<PaginatedResult<Article[]>>) => {
+        this.dataResult = dataResult;
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
+  }
+}
