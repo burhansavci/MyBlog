42a7b472da53125fafd43848759e5fe5d838dc1f
Burhan SAVCI <burhansavci@gmail.com>
2020-05-07 23:42:19 +0300

    Add ArticleForArchiveReturnDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/ArticleForArchiveReturnDto.cs
@@ -0,0 +1,14 @@
+﻿using MyBlog.Core.Entities;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class ArticleForArchiveReturnDto : IDto
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public int PublishYear { get; set; }
+        public int PublishMonth { get; set; }
+        public string MonthName { get; set; }
+        public int CountByMonth { get; set; }
+    }
+}
