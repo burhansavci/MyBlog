07691cf3d37279e3e67373da5a4f626730d81d00
Burhan SAVCI <burhansavci@gmail.com>
2020-05-10 12:12:58 +0300

    Completed article.component

--- angular/src/app/pages/article/article.component.ts
+++ angular/src/app/pages/article/article.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataResult } from 'src/app/models/data-result';
+import { Article } from 'src/app/models/article';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-
-  constructor() { }
+  dataResult: DataResult<Article>;
+  article: Article;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      this.dataResult = data.dataResult;
+      this.article = data.dataResult.data;
+    });
   }
-
 }
