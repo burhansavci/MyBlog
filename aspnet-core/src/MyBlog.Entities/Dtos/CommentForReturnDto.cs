fe466f529424806187da5a9fc9ff904b346e92ef
Burhan SAVCI <burhansavci@gmail.com>
2020-05-16 11:27:37 +0300

    Added CommentForReturnDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/CommentForReturnDto.cs
@@ -0,0 +1,18 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class CommentForReturnDto : IDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContentMain { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int ArticleId { get; set; }
+        public int? ParentId { get; set; }
+        public List<CommentForReturnDto> SubComments { get; set; }
+    }
+}
