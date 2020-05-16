632ff7c9afd89d47aff4be534a4e2b9493edab9d
Burhan SAVCI <burhansavci@gmail.com>
2020-05-16 23:45:18 +0300

    Add articleId input and refreshCommentList method

--- angular/src/app/components/comment/comment.component.ts
+++ angular/src/app/components/comment/comment.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent implements OnInit {
+  @Input() articleId: number;
+  changed: boolean;
   constructor() {}
 
   ngOnInit(): void {}
 
+  refreshCommentList(changed: boolean) {
+    this.changed = changed;
+  }
 }
