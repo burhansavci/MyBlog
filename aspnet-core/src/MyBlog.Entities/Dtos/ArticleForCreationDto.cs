7e7d6a2c044840f8283895808643dabaa8b052f0
Burhan SAVCI <burhansavci@gmail.com>
2020-05-31 22:02:34 +0300

    Added ArticleForCreationDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/ArticleForCreationDto.cs
@@ -0,0 +1,19 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class ArticleForCreationDto : IDto
+    {
+        public string Title { get; set; }
+        public string ContentSummary { get; set; }
+        public string ContentMain { get; set; }
+        public DateTime PublishDate { get; set; }
+        public List<PictureForCreationDto> Pictures { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public string LanguageCode { get; set; }
+
+    }
+}
