ecfea03da0498708d4874c39275ac776f2b0ee9d
Burhan SAVCI <burhansavci@gmail.com>
2020-06-28 18:47:52 +0300

    Add ArticleForUpdateDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/ArticleForUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class ArticleForUpdateDto : IDto
+    {
+        public string Title { get; set; }
+        public string ContentSummary { get; set; }
+        public string ContentMain { get; set; }
+        public DateTime PublishDate { get; set; }
+        public List<PictureForUpdateDto> Pictures { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public int ArticleTranslationId { get; set; }
+        public int Id { get; set; }
+        public string LanguageCode { get; set; }
+
+    }
+}
