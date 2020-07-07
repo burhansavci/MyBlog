6ea0c769340100f188de2a59ae094d1bf4535028
Burhan SAVCI <burhansavci@gmail.com>
2020-07-07 22:28:26 +0300

    Add category resolver

---
+++ angular/src/app/resolvers/category.resolver.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AlertifyService } from '../services/alertify.service';
+import { catchError } from 'rxjs/operators';
+import { Category } from '../models/category';
+import { CategoryService } from '../services/category.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryResolver implements Resolve<Category[]> {
+  pageNumber = 1;
+  pageSize = 5;
+  startPageNumber = 1;
+  endPageNumber = 3;
+
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Category[]> {
+
+    return this.categoryService
+      .getCategories(this.startPageNumber, this.endPageNumber, this.pageSize)
+      .pipe(
+        catchError(() => {
+          this.alertify.error('Problem retrieving categories data');
+          this.router.navigate(['/']);
+          return of(null);
+        })
+      );
+  }
+}
