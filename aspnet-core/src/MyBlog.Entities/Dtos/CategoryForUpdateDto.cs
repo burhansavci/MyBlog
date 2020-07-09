348bbccfc556a0c267ede3ffea6ef028bb4e53a9
Burhan SAVCI <burhansavci@gmail.com>
2020-07-09 21:01:58 +0300

    Add CategoryForUpdateDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/CategoryForUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Core.Entities;
+using System;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class CategoryForUpdateDto : IDto
+    {
+        public int Id { get; set; }
+        public int CategoryTranslationId { get; set; }
+        public string LanguageCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
