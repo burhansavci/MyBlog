3d0ae0cb99e54a4576a4a990fd55d05e3b581a22
Burhan SAVCI <burhansavci@gmail.com>
2020-07-09 22:45:09 +0300

    Add CategoryForDeleteDto

---
+++ aspnet-core/src/MyBlog.Entities/Dtos/CategoryForDeleteDto.cs
@@ -0,0 +1,10 @@
+﻿using MyBlog.Core.Entities;
+
+namespace MyBlog.Entities.Dtos
+{
+    public class CategoryForDeleteDto : IDto
+    {
+        public int Id { get; set; }
+        public int CategoryTranslationId { get; set; }
+    }
+}
