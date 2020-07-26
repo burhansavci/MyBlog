d617ef199ac04b94f5cd680c3437e5db2116cda3
Burhan SAVCI <burhansavci@gmail.com>
2020-07-26 17:44:04 +0300

    Add MyBlogDbContextSeed

---
+++ aspnet-core/src/MyBlog.DataAccess/Concrete/EntityFrameworkCore/Seed/MyBlogDbContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Core.Utilities.Security.Hashing;
+using MyBlog.Entities.Concrete;
+using System.Threading.Tasks;
+
+namespace MyBlog.DataAccess.Concrete.EntityFrameworkCore.Seed
+{
+    public static class MyBlogDbContextSeed
+    {
+        //You can change username,email and password
+        public static async Task SeedUsersAsync(MyBlogDbContext context)
+        {
+            var admin = await context.Users.IgnoreQueryFilters().FirstOrDefaultAsync(x => x.UserName == "myblogadmin" && x.Email == "myblogadminemail");
+            if (admin == null)
+            {
+                byte[] passwordHash, passwordSalt;
+                HashingHelper.CreatePasswordHash("myblogadminpassword", out passwordHash, out passwordSalt);
+                var user = new User
+                {
+                    UserName = "myblogadmin",
+                    Email = "myblogadminemail",
+                    PasswordSalt = passwordSalt,
+                    PasswordHash = passwordHash,
+                };
+                await context.AddAsync(user);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- aspnet-core/src/MyBlog.WebAPI/Program.cs
+++ aspnet-core/src/MyBlog.WebAPI/Program.cs
@@ -1,16 +1,27 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using MyBlog.Business.DependencyResolvers.Autofac;
+using MyBlog.DataAccess.Concrete.EntityFrameworkCore;
+using MyBlog.DataAccess.Concrete.EntityFrameworkCore.Seed;
+using System.Threading.Tasks;
 
 namespace MyBlog.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.GetAutofacRoot().BeginLifetimeScope())
+            {
+                var context = scope.Resolve<MyBlogDbContext>();
+                await context.Database.MigrateAsync();
+                await MyBlogDbContextSeed.SeedUsersAsync(context);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
